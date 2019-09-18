d685cf96933f6fd9ba471af287307ddfa63acc02
Tunde Yakub <tundenasri@gmail.com>
2019-09-18 17:07:51 +0100

    create dropdown select component

---
+++ src/dropdownSelect.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import Select from "react-select";
+import axios from 'axios';
+import './asset/dropdown.css';
+
+const DropdownSelect = () => {
+
+    const setValues = (values) => {
+        console.log(values.value)
+    }
+
+    const [dropdownOption, setDropdownOption] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://www.bitstamp.net/api/v2/trading-pairs-info/')
+        .then(res => {
+            const options = []
+            res.data.map(data => {
+                const select = {
+                    value: data.name,
+                    label: data.name
+                }
+                options.push(select)
+            })
+            setDropdownOption(options)
+        })
+    }, [])
+
+    return ( 
+        <div>
+            <h4> Select currency pair</h4>
+            <div className='selectDropdown'>
+                <Select options={dropdownOption} onChange={(values) => setValues(values)} />
+            </div>
+        </div>
+     );
+}
+ 
+export default DropdownSelect;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import DropdownSelect from './dropdownSelect';
+import './asset/index.css';
 
 const App = () => {
   return (
       <div className='container'>
           <h1 className='header'> Welcome to Order App </h1>
+          <hr/>
+          <DropdownSelect />
       </div>
   )
 };
